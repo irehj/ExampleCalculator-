3fb75a27a7e168a7f65776b6758bca2b5ed7eca9
irehj <irene920609@gmail.com>
2021-07-05 19:57:27 +0900

    최초생성

---
+++ Sample.Simariaji/Sample.Simariaji/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Sample.Simariaji
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+                Console.WriteLine("두개의 숫자를 입력하세요");
+                Console.WriteLine("첫번째 숫자를 입력하세요");
+                string firtsnum = Console.ReadLine();
+                Console.WriteLine("두번째 숫자를 입력하세요");
+            string secondnum = Console.ReadLine();
+            Console.WriteLine("연산자를 입력하세요");
+            string oper = Console.ReadLine();
+            Console.Write("값 : ");
+            int result = 0;
+
+            if (oper == "+")
+            {
+              result = Plus(int.Parse(firtsnum), int.Parse(secondnum));
+            }
+            else if (oper == "-")
+                {
+                result = Minus(int.Parse(firtsnum), int.Parse(secondnum));
+            }
+            else if (oper == "*")
+                {
+                result = Mulit(int.Parse(firtsnum), int.Parse(secondnum));
+            }
+            else if (oper == " /")
+                {
+                result = Div(int.Parse(firtsnum), int.Parse(secondnum));
+            }
+            Console.WriteLine(result);
+        }
+
+                //참조 1개 
+        public static int Plus(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            return result;
+
+        }
+
+        public static int Minus(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+
+        public static int Mulit(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+        public static int Div(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
+    }
+
+
+}
---
+++ Sample.Simariaji/Sample.Simariaji/work.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Sample.Simariaji
+{
+    public class Work
+    {
+        public static void star()
+        {
+        
+            for (int i = 0 ; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine(" ");
+            }            
+
+        }
+    }
+}
