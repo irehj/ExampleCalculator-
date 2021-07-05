a6f77e3a9928cfa280349c31c4fd153681a0f908
irehj <irene920609@gmail.com>
2021-07-05 20:09:48 +0900

    <fix> 컨벤션 맞춤

--- Sample.Simariaji/Sample.Simariaji/Program.cs
+++ Sample.Simariaji/Sample.Simariaji/Program.cs
@@ -7,10 +7,10 @@ namespace Sample.Simariaji
         public static void Main(string[] args)
         {
 
-                Console.WriteLine("두개의 숫자를 입력하세요");
-                Console.WriteLine("첫번째 숫자를 입력하세요");
-                string firtsnum = Console.ReadLine();
-                Console.WriteLine("두번째 숫자를 입력하세요");
+            Console.WriteLine("두개의 숫자를 입력하세요");
+            Console.WriteLine("첫번째 숫자를 입력하세요");
+            string firtsnum = Console.ReadLine();
+            Console.WriteLine("두번째 숫자를 입력하세요");
             string secondnum = Console.ReadLine();
             Console.WriteLine("연산자를 입력하세요");
             string oper = Console.ReadLine();
@@ -19,24 +19,24 @@ namespace Sample.Simariaji
 
             if (oper == "+")
             {
-              result = Plus(int.Parse(firtsnum), int.Parse(secondnum));
+                result = Plus(int.Parse(firtsnum), int.Parse(secondnum));
             }
             else if (oper == "-")
-                {
+            {
                 result = Minus(int.Parse(firtsnum), int.Parse(secondnum));
             }
             else if (oper == "*")
-                {
+            {
                 result = Mulit(int.Parse(firtsnum), int.Parse(secondnum));
             }
             else if (oper == " /")
-                {
+            {
                 result = Div(int.Parse(firtsnum), int.Parse(secondnum));
             }
             Console.WriteLine(result);
         }
 
-                //참조 1개 
+        //참조 1개 
         public static int Plus(int num1, int num2)
         {
             int result = num1 + num2;
